02883cb714820d92225c9da9566cdedc341b4722
Magnetic Duck <zenmags@gmail.com>
2015-06-22 14:16:02 +0000

    oops

--- flight-engine.nix
+++ flight-engine.nix
@@ -6,7 +6,7 @@ writeScript "engine" ''
   while 1; do
     read $a;
     for i in $(echo $a | grep chat); do
-      echo '27276,console,serverWhisper here I am' >> servers/command.txt;
+      echo '27276,console,serverWhisper here I am';
     done;
   done;
 ''  
