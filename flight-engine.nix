fd423b7a5ab748f7e4a054008d74eff2188256e4
Magnetic Duck <zenmags@gmail.com>
2015-06-22 14:15:42 +0000

    made it work

--- flight-engine.nix
+++ flight-engine.nix
@@ -3,6 +3,10 @@
 with pkgs;
 
 writeScript "engine" ''
-  sleep 50;
-  echo '27276,console,serverWhisper here I am' >> servers/command.txt;
+  while 1; do
+    read $a;
+    for i in $(echo $a | grep chat); do
+      echo '27276,console,serverWhisper here I am' >> servers/command.txt;
+    done;
+  done;
 ''  
