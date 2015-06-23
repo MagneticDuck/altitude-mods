6b1017db8b42a5482081727b7b9c06c5762656ca
Magnetic Duck <zenmags@gmail.com>
2015-06-23 23:28:22 +0000

    countdown time

--- flight-club/src/Main.hs
+++ flight-club/src/Main.hs
@@ -2,18 +2,30 @@ module Main where
 
 import Control.Monad (forever, void)
 import System.IO
+import Data.Maybe
 
 import FlightClub.Behaviour
 import FlightClub.ActionEvent
 
 main :: IO ()
 main = 
-  runBehaviour 1 myBehaviour
+  runBehaviour Nothing $ mconcat [sayB, countdownB, putUpB]
 
-myBehaviour :: Behaviour Int
-myBehaviour = chatBehaviour (\(state, str) ->
-  case str of
-    "add" -> (state + 1, [])
-    "show" -> (state, [MessageAction $ show state])
+sayB, countdownB, putUpB :: Behaviour (Maybe Int)
+sayB = Behaviour (\(state, event) ->
+  case event of
+    ClockEvent _ -> 
+      (state, maybeToList $ fmap (MessageAction . show) state)
     _ -> (state, [])
   )
+countdownB = Behaviour (\(state, event) ->
+  case event of
+    ClockEvent _ -> (fmap (subtract 1) state, [])
+    _ -> (state, [])
+  )
+putUpB = chatBehaviour (\(state, str) ->
+  case str of
+    "!countdown" -> (Just 10, [])
+    _ -> (state, [])
+  )
+
