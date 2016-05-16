f7f299599173a8eaeee1e40ebe9380fde0795fcc
Chris Gadzinski <zenmags@gmail.com>
2016-05-16 15:19:46 +0000

    recieved stam's additions

---
+++ default_stam.nix
@@ -0,0 +1,408 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+let
+  mkLauncherConfig = import ./launcher-config.nix { inherit pkgs; };
+
+  mkMod = { launcherConfig ? null, extraMaps ? null, service ? null }:
+    stdenv.mkDerivation {
+      name = "mod";
+      
+      phases = "installPhase";
+
+      installPhase = ''
+        mkdir -p $out
+
+        ${lib.optionalString (! isNull launcherConfig) ''
+          mkdir -p $out/servers/
+          cp ${launcherConfig} $out/servers/launcher_config.xml 
+        ''}
+
+        mkdir -p $out/maps/
+        ${lib.concatMapStrings (map:"cp ${map.src} $out/maps/${map.name}\n") extraMaps}
+
+        ${lib.optionalString (! isNull service) ''
+          cp ${service} $out/run
+        ''}
+      '';
+    };
+
+  tbdRabbit = 
+    fetchurl {
+      name = "tbd_rabbit.altx";
+      url = "https://www.dropbox.com/s/yuh78tf45sjq4tl/tbd_rabbit.altx?dl=0";
+      sha256 = "0ms5ydaj73nvavvfsm2p0cdxvj64qjlfqi21c45axrcsmixhyppq";
+    };
+
+  mangoLobby = 
+    fetchurl {
+      name = "tbd_lobby.altx";
+      url = "http://altitudegame.com/map/mapDownload?m=4d63a8cb-26b5-45a8-b478-6a47aaa7270c";
+      sha256 = "1h03ra2wi26v8k2j8sjbhhc6grgb9l4ykfxcqr9frby3pgl52ngs";
+    };
+
+  jonusArrowTbd =
+    fetchurl {
+      name = "tbd_arrow_new.altx";
+      url = "http://magnetic.uk.to/misc/maps/tbd_arrow.altx";
+      sha256 = "0kqk8d8yb9d9n1wvcy9ij53mlvmcmxpxgskwrh6smgrs3bs2v73l";
+    };
+
+  jonusArrowTbd2 =
+    fetchurl {
+      name = "tbd_arrow2.altx";
+      url = "http://magnetic.uk.to/misc/maps/tbd_arrow2.altx";
+      sha256 = "1ldsfqmkf4k4bsfbnckl6lwac8vqip6bxpplr5g26s4vlkij9vfa";
+    };
+
+  jonusArrowBall =
+    fetchurl {
+      name = "ball_arrow.altx";
+      url = "http://magnetic.uk.to/misc/maps/ball_arrow.altx";
+      sha256 = "1ad0pg0mqq4ql39i7wl243wq9l3d44mkdzq4w7mdz9vdgxrdavjp";
+    };
+
+  tbgLobby =
+    fetchurl {
+      name = "lobby_tbg.altx";
+      url = "https://www.dropbox.com/s/ns9e4f9hzgq3sun/lobby_tbg.altx?dl=0";
+      sha256 = "1cxbl7cwdn29k91hihhc96v4xzsp292vajca42w1p1irjv3xjqmq";
+    };
+    
+  ballAntre =
+    fetchurl {
+      name = "ball_antre_pb.altx";
+      url = "http://magnetic.uk.to/misc/maps/ball_antre_pb.altx";
+      sha256 = "1vpa0lymcrk7zr8kyri5n5bmrf3akd9lmng5sj7dlbbp5aa01fk7";
+    };
+    
+  ballCave =
+    fetchurl {
+      name = "ball_cave_pb.altx";
+      url = "http://magnetic.uk.to/misc/maps/ball_cave_pb.altx";
+      sha256 = "03zsnw9diqqagq1pmcsc1m86zk99yilhlmqh03hhk1kwv79if8ls";
+    };
+    
+  ballIce =
+    fetchurl {
+      name = "ball_ice_pb.altx";
+      url = "http://magnetic.uk.to/misc/maps/ball_ice_pb.altx";
+      sha256 = "1ny224z5alp1p8h1yqs2yn78k3k66wp5ffs0kp84zwfsdk63wml7";
+    };
+    
+  ballLostcity2 =
+    fetchurl {
+      name = "ball_lostcity2_pb.altx";
+      url = "http://magnetic.uk.to/misc/maps/ball_lostcity2_pb.altx";
+      sha256 = "0fmila3kdkmkr1j4sf7qzl4d41kad148x7qxqw3lrb756bnf3h7g";
+    };
+    
+  ballSnow =
+    fetchurl {
+      name = "ball_snow_pb.altx";
+      url = "http://magnetic.uk.to/misc/maps/ball_snow_pb.altx";
+      sha256 = "0cg6v1gkmm891jgss1sdp3a59j4nk804ji29ndcap03qvzbnb6sa";
+    };
+	
+  1lhroids =
+  fetchurl {
+    name = "1lh_asteroids.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=a1393da7-3494-423e-bcb4-9344fbcb668c";
+    sha256 = "0i8xf2g0p8sqvpn201z21bscaf0nhfk8famxc6w1qnwjzbhz13cf";
+  };
+
+  1lhroids2 = 
+    fetchurl {
+    name = "1lh_asteroids2.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=40471826-1bd7-4873-85f1-17065fd2cd08";
+    sha256 = "0njvhc8hxdq847mmyj2zgam7r2ivqvcnpvr1x7a8a64aq4i4if9f";
+  };
+
+  1lhcave =
+    fetchurl {
+    name = "1lh_cave.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=97dd6ded-7ab1-4071-b79a-4d3f615afd83";
+    sha256 = "1axnkrz7ayj2wcsk6l82w9yzf0w8m9w0jbhv45nj0009ar75d6kn";
+  };
+
+  1lhcave2 =
+    fetchurl {
+    name = "1lh_cave2.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=04aa4818-f94a-4e7c-84d6-037281188007";
+    sha256 = "0hxv5zd7ys0y9ikccbh1wp197l1h4yghgml0j1n6npaam2b9rk6p";
+  };
+
+  1lhclocktower =
+    fetchurl {
+    name = "1lh_clocktower.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=bf755e2f-e7d8-44b1-9bb0-48e3a74a9be1";
+    sha256 = "1sskhyinj1xpid9xi0v8gaqbjdpx35qmzzis01a136bhj4hn2gr0";
+  };
+
+  1lhcore =
+    fetchurl {
+    name = "1lh_core.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=032720b6-460d-40ba-8145-99c5fcabd6af";
+    sha256 = "0k41wac2jg0dz2vclxcz45p1rwvalgzhslrriqiamwban3gvvgvy";
+  };
+
+  1lhfallout =
+    fetchurl {
+    name = "1lh_fallout.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=68b3da8c-8129-4ba5-ae95-7ee2124f99fc";
+    sha256 = "16vm7s8ldf1lnlbhxb34mqxs5b7p0s5sdd6rdg80nbnv42fz2w78";
+  };
+
+  1lhgrotto =
+    fetchurl {
+    name = "1lh_grotto.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=9379137f-29fd-49bb-bd23-b725d6d02661";
+    sha256 = "19if412yy54z6sr3mrydb7vzdwxfv9ba5jib96y9b4n776sl2jsm";
+  };
+
+  1lhlocomotion =
+    fetchurl {
+    name = "1lh_locomotion.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=32dc5076-c0d3-4761-908b-3fc2a29d5895";
+    sha256 = "1jn9lxsczj5snkqwk0h7v508w5vzgq67vmbj0nkdcc73zg9p5mpp";
+  };
+
+  1lhmayhem =
+    fetchurl {
+    name = "1lh_mayhem.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=7dab829f-c85b-415d-9b57-c17d3b3c125a";
+    sha256 = "1l3hyq4miqjqbnyn597w5983mr2jcv8rs7rkbpqrwrk8lcp18xfj";
+  };
+
+  1lhmayhem2 =
+    fetchurl {
+    name = "1lh_mayhem2.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=a970db5d-4009-41ef-9f93-11631d1cba0b";
+    sha256 = "15jcrhm6ard5aqnvkzvy0vcq70m55v4hxciy1yj6c0h8cx70qwa0";
+  };
+
+  1lhmaze =
+    fetchurl {
+    name = "1lh_maze.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=cf39a712-61e8-4a65-ac00-b2825a7aee03";
+    sha256 = "1wr1i66150q0wcymsmfq90hwrd4y6mm3shf2bn6mzv72ssfmlbaw";
+  };
+
+  1lhmetropolis =
+    fetchurl {
+    name = "1lh_metropolis.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=ee74f4aa-a13d-4b75-8b6f-653a6e04b78a";
+    sha256 = "189qpraqsmjb6wbrkkf61sfy8dvgxijpd8gphasxsqjqhysy0n32";
+  }
+
+  1lhmiddleground =
+    fetchurl {
+    name = "1lh_middleground.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=cf6e49a4-4645-4920-b075-7c1d2dc999ff";
+    sha256 = "0k72jfmv71yyslvq1bkvdwxfy2l0211ddpdsp9r7zjv82gc79wi3";
+  };
+
+  1lhplanepark =
+    fetchurl {
+    name = "1lh_planepark.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=1f5bdc36-d2de-4c73-9884-3a0fd5d0e1b5";
+    sha256 = "1j6754jgwls8v7nbi04a4p61sxcbx3hp364w5ffscxwhdz1j6vq2";
+  };
+
+  1lhslick =
+    fetchurl {
+    name = "1lh_slick.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=e65afeda-4795-4846-9fe7-401158072ee2";
+    sha256 = "09514j2klydw9976kiggsyvma1r646rpa4sj7j1wahqc1a0wkz7w";
+  };
+
+  1lhtwisted =
+    fetchurl {
+    name = "1lh_twisted.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=fbc3c202-47b3-445d-8b0b-5388854ee81a";
+    sha256 = "1wq1rg0yhj5yd47frhnf1hs69fvyjzhh0m7z58pgjs8rgqcn9vnh";
+  };
+
+  1lhunderpark =
+    fetchurl {
+    name = "1lh_underpark.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=b4b4a0aa-86bb-4e0f-b76b-50fa440825cb";
+    sha256 = "14pa6wlgbd0riv7ivls9kz6m04i3hdbk0x8g0b0pal8kva3wgqxs";
+  };
+
+  1lhwoods =
+    fetchurl {
+    name = "1lh_woods.altx";
+    url = "http://altitudegame.com/map/mapDownload?m=11245122-14b1-4a21-8c20-722f1328fe03";
+    sha256 = "040m1q8fiqvm1v1vd6m966vkfwwyxal0j591ydl6kj3736dhi86z";
+  };
+	
+  balltron =
+    fetchurl {
+	  name = "ball_tron.altx";
+	  url = "http://altitudegame.com/map/mapDownload?m=bc3e62f1-075e-48b7-83a3-fa997da88708";
+	  sha256 = "1p28gdnn6w4cm0k3j8lbqgfiq2c36v23c40nig239fd2cayy3s1a";
+	};
+  
+
+  flightClub = { mkDerivation, base, stdenv, json }:
+    mkDerivation {
+      pname = "flight-club"; version = "0.1.0.0";
+      src = ./flight-club;
+      isLibrary = false; isExecutable = true;
+      buildDepends = [ base json ];
+      license = stdenv.lib.licenses.publicDomain;
+    };
+
+  haskellService = admins:
+    writeScript "flight-club-service" ''
+      ${haskellPackages.callPackage flightClub {}}/bin/flight-club ${admins}
+    '';  
+
+  admins = 
+    [ "5640761e-f165-4f40-b3d6-3e3167dd767d" # duck
+      "faa8061c-5cd6-43bb-b389-561be0792b33" # kafka
+      "55dd4345-53e2-4a5d-b6e1-7ca357c7337d" # label
+      "7c29079d-5ead-4136-8b1d-467513350b79" # demv
+      "b61aa791-7cc3-44bf-bba0-1c7faa9009a9" # toma 
+      "31456ecc-b5f2-4615-88b4-c4aeadcbf0a7" # golden
+      "bf60335d-784f-4f31-98f8-3e0bb591b8c2" # moxy
+      "8612de03-f66c-4281-9d7b-8b751837a3c6" # jonus
+      "2f2101b2-bfda-4f95-8f6d-a590992d8108" # max
+      "92f40cad-d09f-48b4-b165-5d3c3d97f26c" # brutal
+      "02a33655-b2eb-42c2-bc88-fff24240f0d8" # phyx
+      "0aa6b54c-52b4-4fa7-b2b4-0dc0c8350dd0" # paiza
+      "d2ade87e-09a6-4ff9-af7b-9ae9f58fd570" # elusive
+      "f4d0b170-2877-4a92-90eb-eb950a57c636" # stam
+	  "c9e24c41-292d-4d40-b76c-230d8f30ef32" # stam2
+      "5ec76fe2-7074-4926-8873-a0e2bf2d9ba5" # dmcm
+      "ba9e19db-2cec-4c01-b984-14ca9313f9ff" # rawr
+    ];
+
+  tbgAdmins = 
+    [ "5640761e-f165-4f40-b3d6-3e3167dd767d" # duck
+      "f4d0b170-2877-4a92-90eb-eb950a57c636" # stam
+      "c9e24c41-292d-4d40-b76c-230d8f30ef32" # stam2
+	  "2cc35ae9-dd99-413b-bbd3-2e1a4ac5b024" # ring
+      "e9ff8bb7-ca22-4cc6-b45b-187db4697e9c" # mani to ba
+      "2ac67d3a-3103-4443-9fb4-1d72219ddb88" # nuffhuff
+	  "25c28e39-8c93-4adb-91c0-783cb9d75959" # nick
+    ];
+
+  adminFile = customAdmins:
+    writeTextFile {
+      name = "adminfile"; text = lib.concatStringsSep "\n" customAdmins;
+    };
+
+  biellMaps = fetchFromGitHub {
+    owner = "biell";
+    repo = "alti-maps";
+    rev = "c45cd6d30925e1c09dc3826d1b1e0aa3df4c3bca";
+    sha256 = "1y1yabmkrkkaf4jzsfhzdvxicg1hi3zq1y04jjlmwxhlhl1wgww4";
+  };
+
+  getBiellMap = mapName:
+    runCommand "biell_map" {maps = biellMaps;} ''
+      cp $maps/maps/${mapName} $out
+    '';
+  
+in
+
+{
+  null = mkMod { };
+ 
+  default =
+    mkMod {
+      launcherConfig =
+        mkLauncherConfig {
+          name = "FLIGHT CLUB";
+          port = "27276";
+          password = "ruleone";
+          players = "40";
+          lobby = "lobby_club";
+          maps = ["|tbd|" "|1dm|" "|ball|" "|1de|" "|tdm|" "tbd_arrow" "ball_arrow" "tbd_arrow2" "1lh_asteroids" "1lh_asteroids2" "1lh_cave" "1lh_cave2" 
+				  "1lh_clocktower" "1lh_core" "1lh_fallout" "1lh_grotto" "1lh_locomotion" "1lh_mayhem" "1lh_mayhem2" "1lh_maze" "1lh_metropolis" 
+				  "1lh_middleground" "1lh_planepark" "1lh_slick" "1lh_twisted" "1lh_underpark" "1lh_woods"];
+          admins = admins;
+        };
+      service = (haskellService (adminFile admins));
+      extraMaps = [
+        {src = mangoLobby; name = "lobby_club.altx";}
+        {src = jonusArrowBall; name = "ball_arrow.altx";}
+        {src = jonusArrowTbd; name = "tbd_arrow.altx";}
+        {src = jonusArrowTbd2; name = "tbd_arrow2.altx";}
+
+        {src = 1lhroids; name = "1lh_asteroids.altx";}
+        {src = 1lhroids2; name = "1lh_asteroids2.altx";}
+        {src = 1lhcave; name = "1lh_cave.altx";}
+        {src = 1lhcave2; name = "1lh_cave2.altx";}
+        {src = 1lhclocktower; name = "1lh_clocktower.altx";}
+        {src = 1lhcore; name = "1lh_core.altx";}
+        {src = 1lhfallout; name = "1lh_fallout.altx";}
+        {src = 1lhgrotto; name = "1lh_grotto.altx";}
+        {src = 1lhlocomotion; name = "1lh_locomotion.altx";}
+        {src = 1lhmayhem; name = "1lh_mayhem.altx";}
+        {src = 1lhmayhem2; name = "1lh_mayhem2.altx";}
+        {src = 1lhmaze; name = "1lh_maze";}
+        {src = 1lhmetropolis; name = "1lh_metropolis.altx";}
+        {src = 1lhmiddleground; name = "1lh_middleground";}
+        {src = 1lhplanepark; name = "1lh_planepark.altx";}
+        {src = 1lhslick; name = "1lh_slick.altx";}
+        {src = 1lhtwisted; name = "1lh_twisted.altx";}
+        {src = 1lh_underpark; name = "1lh_underpark.altx";}
+        {src = 1lhwoods; name = "1lh_woods.altx";} 
+      ];
+    };
+  
+  tbg =
+    mkMod {
+      launcherConfig =
+        mkLauncherConfig {
+          name = "{TBG} Official Training Server";
+          port = "27277";
+          password = "discord";
+          rcon = "chickensun123";
+          players = "40";
+          lobby = "lobby_tbg";
+          maps = ["|1dm|" "|ball|" "|1de|" "|tdm|" "ball_arrow" "ball_race_asteroids" "ball_race_eastern_creek" "ball_antre_pb" "ball_cave_pb"
+                  "ball_ice_pb" "ball_lostcity2_pb" "ball_snow_pb" "1lh_asteroids" "1lh_asteroids2" "1lh_cave" "1lh_cave2" "1lh_clocktower" 
+				  "1lh_core" "1lh_fallout" "1lh_grotto" "1lh_locomotion" "1lh_mayhem" "1lh_mayhem2" "1lh_maze" "1lh_metropolis" 
+				  "1lh_middleground" "1lh_planepark" "1lh_slick" "1lh_twisted" "1lh_underpark" "1lh_woods" "ball_tron"];
+          admins = tbgAdmins;
+        };
+      extraMaps = [
+        {src = tbgLobby; name = "lobby_tbg.altx";}
+        {src = jonusArrowBall; name = "ball_arrow.altx";}
+        {src = getBiellMap "ball_race_asteroids.altx"; name = "ball_race_asteroids.altx";}
+        {src = getBiellMap "ball_race_eastern_creek.altx"; name = "ball_race_eastern_creek.altx";}
+        {src = ballAntre; name = "ball_antre_pb.altx";}
+        {src = ballCave; name = "ball_cave_pb.altx";}
+        {src = ballIce; name = "ball_ice_pb.altx";}
+        {src = ballLostcity2; name = "ball_lostcity2_pb.altx";}
+        {src = ballSnow; name = "ball_snow_pb.altx";}
+
+        {src = 1lhroids; name = "1lh_asteroids.altx";}
+        {src = 1lhroids2; name = "1lh_asteroids2.altx";}
+        {src = 1lhcave; name = "1lh_cave.altx";}
+        {src = 1lhcave2; name = "1lh_cave2.altx";}
+        {src = 1lhclocktower; name = "1lh_clocktower.altx";}
+        {src = 1lhcore; name = "1lh_core.altx";}
+        {src = 1lhfallout; name = "1lh_fallout.altx";}
+        {src = 1lhgrotto; name = "1lh_grotto.altx";}
+        {src = 1lhlocomotion; name = "1lh_locomotion.altx";}
+        {src = 1lhmayhem; name = "1lh_mayhem.altx";}
+        {src = 1lhmayhem2; name = "1lh_mayhem2.altx";}
+        {src = 1lhmaze; name = "1lh_maze";}
+        {src = 1lhmetropolis; name = "1lh_metropolis.altx";}
+        {src = 1lhmiddleground; name = "1lh_middleground";}
+        {src = 1lhplanepark; name = "1lh_planepark.altx";}
+        {src = 1lhslick; name = "1lh_slick.altx";}
+        {src = 1lhtwisted; name = "1lh_twisted.altx";}
+        {src = 1lh_underpark; name = "1lh_underpark.altx";}
+        {src = 1lhwoods; name = "1lh_woods.altx";}
+        {src = balltron; name = "ball_tron.altx";}
+      ];
+    };
+
+  inherit haskellEngine;
+}
